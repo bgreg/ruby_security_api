c1f8fabb6ef85e3493181566e52336193559f90d
Greg Mcguirk <gregmcguirk@Gregs-MacBook-Pro.local>
2014-09-14 12:02:53 -0700

    master - rake spec comming along

--- lib/tasks/download_exposures.rake
+++ lib/tasks/download_exposures.rake
@@ -1,6 +1,16 @@
-namespace :db do
-  desc "Download Newest exposure data"
-  task download_exposures: :environment do
+require 'open-uri'
 
+module ExposureLoader
+  def recent_exposure_uri
+    "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-modified.xml"
   end
 end
+namespace :db do
+  desc "Download Newest exposure data"
+  task download_recent_exposures: :environment do
+    include ExposureLoader
+    doc = Nokogiri::XML(open(recent_exposure_uri))
+  end
+
+end
+
--- spec/lib/download_exposures_spec.rb
+++ spec/lib/download_exposures_spec.rb
@@ -4,9 +4,19 @@ describe ':download_exposures' do
   before { RubySecurityApi::Application.load_tasks }
 
   it "can run without exception" do
+    stub_request(:get, "http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-modified.xml").
+      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+      to_return(:status => 200, :body => "", :headers => {})
+
     expect {
-      Rake::Task['db:download_exposures'].invoke
+      Rake::Task['db:download_recent_exposures'].invoke
     }.not_to raise_exception
+  end
+
+  it "can create 1 exposure per xml entry" do
+    expect{
+      Rake::Task['db:download_recent_exposures'].invoke
+    }.to change{Exposure.count}.by(194)
   end
 
 end
